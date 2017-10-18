35fbb1663bca5c50b68d570c5588f8fc9af3345e
Breno Henrique <breno@kazap.com.br>
2017-10-18 15:58:32 -0200

    importado para o arquivo de comunicacao para o server.js para configurar uma conexao com banco de dados MongoDB, colocado para exibir se deu certo a conexao ou deu erro e informar o erro

--- server.js
+++ server.js
@@ -5,11 +5,22 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import config from './config/DB';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB).then(
+  () => { console.log('Database is connected') },
+  err => { console.log('Can not connect to the Database ' err) }
+);
 
 const app = express();
 
-var port = process.env.PORT || 4000;
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
 
-var server = app.listen( () => {
+const port = process.env.PORT || 4000;
+
+const server = app.listen( () => {
   console.log('Listening on port ' + port);
 });
