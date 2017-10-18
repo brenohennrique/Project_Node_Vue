0bc53dd1a0b6c995c40a36cae17df7525c0866e2
Breno Henrique <breno@kazap.com.br>
2017-10-18 15:15:27 -0200

    criado componente de criar perfil

---
+++ src/components/CreateProfile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Cadastro de Perfil</h1>
+    <form>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Perfil Descição:</label>
+            <input type="text" class="form-control" v-model="profile.title">
+          </div>
+        </div>
+      </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Adicionar Perfil</button>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return{
+        profile: {}
+      }
+    },
+    methods: {
+      addProfile(){
+
+      }
+    }
+    
+  }
+</script>
