b66e47a4b782c416be90db941c272f5927382e73
Breno Henrique <breno@kazap.com.br>
2017-10-19 14:59:24 -0200

    criado componente displayPerfil para listar todos perfils e poder realizar o CRUD

---
+++ src/components/DisplayProfile.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h1>Perfils</h1>
+
+        <div class="row">
+          <div class="col-md-10"></div>
+          <div class="col-md-2">
+            <router-link :to="{ name: 'CreateProfile' }" class="btn btn-primary">Adicionar Perfil</router-link>
+          </div>
+        </div><br />
+
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <td>ID</td>
+                <td>Descrição</td>
+                <td>Ações</td>
+            </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="profile in profiles">
+                    <td>{{ profiles._id }}</td>
+                    <td>{{ profiles.name }}</td>
+                    <td><router-link :to="{name: 'EditProfile', params: { id: profiles._id }}" class="btn btn-primary">Editar</router-link></td>
+                    <td><button class="btn btn-danger" v-on:click="deleteProfile(profiles._id)">Deletar</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                profiles: []
+            }
+        },
+        created: function()
+        {
+            this.fetchProfiles();
+        },
+        methods: {
+            fetchProfiles()
+            {
+              let uri = 'http://localhost:4000/profiles';
+              this.axios.get(uri).then((response) => {
+                  this.profiles = response.data;
+              });
+            },
+            deleteProfile(id)
+            {
+              let uri = 'http://localhost:4000/profiles/delete/'+id;
+              this.profiles.splice(id, 1);
+              this.axios.get(uri);
+            }
+        }
+    }
+</script>
