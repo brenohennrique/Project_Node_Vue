90dbb068a44b46532be1408a10804e72669640c1
Breno Henrique <breno@kazap.com.br>
2017-10-20 11:55:25 -0200

    criado model de usuario

---
+++ models/User.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Define collection and schema for User
+var User = new Schema({
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  profile: {
+    type: String
+  }
+  }, {
+    collection: 'users'
+  });
+
+  module.exports = mongoose.model('User', User);
